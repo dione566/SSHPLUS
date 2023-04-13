66ccf4173b2fe91e5cb7381fbe52aab622f0f97e
dione566 <77499190+dione566@users.noreply.github.com>
2023-04-13 17:55:12 -0400

    Update up1.sh

--- Modulos/up1.sh
+++ Modulos/up1.sh
@@ -1,1 +1,60 @@
-novo
+{
+  "Version": "1.0.1",
+  "ReleaseNotes": "MELHORIAS ENCONTRADAS!",
+  "UrlUpdate": "https://raw.githubusercontent.com/sudosalve/sshpluss/master/Modulos/up2.sh",
+  "Sms": "https://raw.githubusercontent.com/dione566/sms/master/sms.sh",
+  "EmailFeedback": "",
+  "UrlContato": "https://wa.me/5566992527471",
+  "UrlTermos": "https://sudosalve.github.io/GOMESNET_site/termos.html",
+  "CheckUser": "true",
+  "Udp": [
+    {
+      "Porta": "7300"
+    }
+  ],
+  "Servers": [
+    {
+      "Name": "GOMESNET",
+      "TYPE": "premium",
+      "FLAG": "br.png",
+      "ServerIP": "salve.gomesssh.store",
+      "CheckUser": "http://salve.gomesssh.store:5454/checkUser",
+      "ServerPort": "22",
+      "SSLPort": "443",
+      "USER": "",
+      "PASS": ""
+    }
+  ],
+   "Networks": [
+    {
+      "Name": "01 - Version: 1.4.77",
+      "FLAG": "vivo",
+      "Payload": "GET / HTTP/1.1[crlf]Host:[rotate= 981i31n148ya9403x6.speedup.mobi;q31k171216e3r84g38.speedup.mobi;136i413d9fe11623r8.speedup.mobi;d339120466v8rf241m.speedup.mobi;188348191lx76t52tm.speedup.mobi;639p2s66i4142m158u.speedup.mobi;8g96a3831s82156q2j.speedup.mobi;1563278a4k3wc10l11.speedup.mobi;n46f1114u86vg61959.speedup.mobi;12833v605t84i0r81o.speedup.mobi;s1.gomesssh.store;s2.gomesssh.store][crlf]Connection: Upgrade[crlf]Upgrade: Websocket[crlf][crlf]",
+      "SNI": "",
+      "TlsIP": "",
+      "ProxyIP": "13.227.126.65",
+      "ProxyPort": "80",
+      "Info": "Proxy"
+    },
+    {
+      "Name": "02 - Version: 1.4.77",
+      "FLAG": "vivo",
+      "Payload": "GET / HTTP/1.1[crlf]Host:[rotate= 981i31n148ya9403x6.speedup.mobi;q31k171216e3r84g38.speedup.mobi;136i413d9fe11623r8.speedup.mobi;d339120466v8rf241m.speedup.mobi;188348191lx76t52tm.speedup.mobi;639p2s66i4142m158u.speedup.mobi;8g96a3831s82156q2j.speedup.mobi;1563278a4k3wc10l11.speedup.mobi;n46f1114u86vg61959.speedup.mobi;12833v605t84i0r81o.speedup.mobi;s1.gomesssh.store;s2.gomesssh.store][crlf]Connection: Upgrade[crlf]Upgrade: Websocket[crlf][crlf]",
+      "SNI": "",
+      "TlsIP": "",
+      "ProxyIP": "recarga.vivo.com.br",
+      "ProxyPort": "80",
+      "Info": "Proxy"
+    },
+    {
+       "Name": "03 - 1Store 1.4.77 >",
+      "FLAG": "vivo",
+      "Payload": "GET ssl://salve.gomesssh.store HTTP/1.1[crlf]Host: none[crlf]Upgrade: Websocket[crlf][crlf]",
+      "SNI": "salve.gomesssh.store",
+      "TlsIP": "proxy.gomesssh.store",
+      "ProxyIP": "proxy.gomesssh.store",
+      "ProxyPort": "443",
+      "Info": "Tlsws"
+    }
+  ]
+}
