9f031a5bd59ba71e33bbe29cf53298540dfe1847
dione566 <77499190+dione566@users.noreply.github.com>
2023-04-28 13:57:00 -0400

    Update up1.sh

--- Modulos/up1.sh
+++ Modulos/up1.sh
@@ -1,5 +1,5 @@
 {
-  "Version": "1.0.13",
+  "Version": "1.0.14",
   "ReleaseNotes": "MELHORIAS ENCONTRADAS!",
   "UrlUpdate": "https://raw.githubusercontent.com/dione566/SSHPLUS/master/Modulos/up1.sh",
   "Sms": "https://raw.githubusercontent.com/dione566/sms/master/sms.sh",
@@ -27,7 +27,7 @@
   ],
    "Networks": [
     {
-      "Name": "01 - Version: 1.0.12",
+      "Name": "01 - Version: 1.0.14",
       "FLAG": "vivo",
       "Payload": "GET / HTTP/1.1[crlf]Host:[rotate= 4198ud8qi42824r616.speedup.mobi;611085w62cl608re61.speedup.mobi;5r1745h1s2802bs686.speedup.mobi;0m6262j8367f0513ls.speedup.mobi;5j46v104l0282h682w.speedup.mobi;155p4780628qw1dq16.speedup.mobi;s1.gomesssh.store;s2.gomesssh.store][crlf]Connection: Upgrade[crlf]Upgrade: Websocket[crlf][crlf]",
       "SNI": "",
@@ -37,7 +37,7 @@
       "Info": "Proxy"
     },
     {
-      "Name": "02 - Version: 1.0.12",
+      "Name": "02 - Version: 1.0.14",
       "FLAG": "vivo",
       "Payload": "GET / HTTP/1.1[crlf]Host:[rotate= 4198ud8qi42824r616.speedup.mobi;611085w62cl608re61.speedup.mobi;5r1745h1s2802bs686.speedup.mobi;0m6262j8367f0513ls.speedup.mobi;5j46v104l0282h682w.speedup.mobi;155p4780628qw1dq16.speedup.mobi;s1.gomesssh.store;s2.gomesssh.store][crlf]Connection: Upgrade[crlf]Upgrade: Websocket[crlf][crlf]",
       "SNI": "",
@@ -47,7 +47,7 @@
       "Info": "Proxy"
     },
     {
-       "Name": "03 - 1Store 1.0.12 >",
+       "Name": "03 - Proxy >",
       "FLAG": "vivo",
       "Payload": "GET ssl://salve.gomesssh.store HTTP/1.1[crlf]Host: none[crlf]Upgrade: Websocket[crlf][crlf]",
       "SNI": "salve.gomesssh.store",
